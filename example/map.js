91507ce1aec07ff8619d95aa2ed5fc89c2aecfe9
Dominic Smith <Smith@threemenwise.com>
2011-06-03 21:09:49 +1000

    added example pages

---
+++ example/map.js
@@ -0,0 +1,74 @@
+GRID_SIZE = 10
+
+function Node(x,y){
+	this.x = x || 0.0;
+	this.y = y || 0.0;
+	this.walkable = true;
+	this.distance = function(node){
+		var dx = this.x - node.x;
+		var dy = this.y - node.y;
+		return Math.sqrt(dx*dx + dy*dy);
+	};	//return number
+	this.walkable = function() {
+		return this.walkable;
+	};
+	this.toggle = function() {
+		this.walkable = !this.walkable
+	};
+	this.draw = function(ctx, color) {
+		bw = this.walkable ? 'white' : 'black';
+		ctx.fillStyle = color || bw;
+		ctx.fillRect(x*GRID_SIZE,y*GRID_SIZE,GRID_SIZE,GRID_SIZE)
+	};
+}
+
+function Map(w,h) {
+	this.width = w = w || 10;
+	this.height = h = h || 10;
+	this.grid = [];
+	for (var i = 0; i < w; i++) {
+		this.grid[i] = []
+		for (var j = 0; j < h; j++) {
+			this.grid[i][j] = new Node(i,j)
+		};
+	};
+	this.nodeAt = function(x,y){
+		if (0<=x && x < this.width &&
+				0<=y && y < this.height){
+			return this.grid[x][y];	
+		}else{
+			return null;
+		}
+	}
+	this.draw = function(ctx) {
+		for (var i = 0; i < this.grid.length; i++) {
+			for (var j = 0; j < this.grid[i].length; j++) {
+				this.grid[i][j].draw(ctx);
+			};
+		};
+	};
+	this.randomiz = function() {
+		for (var i = 0; i < this.width; i++) {
+			for (var j = 0; j < this.height; j++) {
+
+				this.grid[i][j].walkable = Math.round(Math.random()) == true
+			};
+		};
+	};
+}
+
+diag_adj = function(node){
+	var out = [];
+	var a = [[-1,-1],[0,-1],[1,-1],
+			 [-1, 0],       [1, 0],
+			 [-1, 1],[0, 1],[1, 1]]
+	for(var p in a){
+		px = node.x + a[p][0]
+		py = node.y + a[p][1]
+		adjnode = map.nodeAt(px,py)
+		if(adjnode && adjnode.walkable){
+			out.push(adjnode);
+		}
+	}
+	return out;
+};
---
+++ example/pathfinder.js
@@ -0,0 +1,103 @@
+//function Node(){
+//	this.distance = function(node){};	//return number
+//	this.ajacent = function() {}; // return ajacent walkable nodes
+//}
+
+function PathFinder(options){
+	this.lastclist = [];
+	var adjnodesf;
+
+	if(options.adj){
+		adjnodesf = function(){return options.adj(this.node);};
+	}else{
+		adjnodesf = false;
+	}
+
+	PathNode = function(node, parent){
+		this.parent = parent;
+		this.node = node;
+
+		this.F = function(goal){
+			return this.G() + this.H(goal);
+		};
+		this.G = function(){
+			if (this.parent === undefined){
+				return 0;
+			}else{
+				return this.parent.G() + this.node.distance(this.parent.node);
+			}
+		};
+		this.H = function(goal){
+			return this.node.distance(goal);
+		};
+		this.path = function(){
+			if (this.parent === undefined){
+				return [this.node];
+			}else{
+				var path = this.parent.path();
+				path.push(this.node);
+				return path;
+			}
+		};
+		this.adj = adjnodesf || function(){return this.node.ajacent();};
+	};
+
+}
+PathFinder.prototype.findpath = function(start, goal){
+	var openlist = [];
+	var closedlist = this.lastclist = [];
+
+	
+	var nOpen = function(node){
+		openlist.push(node);
+	};
+	var nNext = function(){
+		var best = {F : function(){return Number.MAX_VALUE;}};
+		for (var i = 0; i < openlist.length; i++) {
+			if(openlist[i].F(goal) < best.F(goal)){
+				best = openlist[i];
+			}
+		}
+		return best;
+	};
+	var nClose = function(node){
+		closedlist.push(node);
+		var i = openlist.indexOf(node);
+		openlist.splice(i,1);
+	};
+	var inList = function(ent, i, ary) {
+		return this == ent.node;
+	};
+
+
+	var current = new PathNode(start);
+	
+
+	nOpen(current);
+
+	do{
+		nClose(current);
+		var adj = current.adj();
+		for(var n in adj){
+			if(!closedlist.some(inList,adj[n])){
+				var oents = openlist.filter(inList,adj[n]);
+				if(oents.length > 0){
+					old = oents[0];
+					nuw = new PathNode(adj[n], current);
+					if(old.G() > nuw.G()){
+						old.parent = current;
+					}
+				}else{
+					nOpen(new PathNode(adj[n], current));
+				}
+			}
+		}
+
+		if(closedlist.some(inList,goal)){
+			return closedlist.filter(inList,goal)[0].path();
+		}
+
+		current = nNext();
+	}while(openlist.length > 0);
+	return false;
+};
