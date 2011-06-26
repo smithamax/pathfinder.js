106b149ab511d2646b95a9ded98c62e923a361d5
Dominic Smith <Smith@threemenwise.com>
2011-06-26 15:47:59 +1000

    updated pathfinder.js,
    PathNodes now inherit their functions, should imporve performance... maybe.

--- pathfinder.js
+++ pathfinder.js
@@ -14,32 +14,34 @@ function PathFinder(options){
 		this.adjFunc = function(node){return node.ajacent();};
 	}
 
-	PathNode = function(node, parent){
-		var _g = null;
+	var PathNode = function(node, parent){
+		this._g = null;
 		this.parent = parent;
 		this.node = node;
+	};
 
-		this.F = function(goal){
+	PathNode.prototype = {
+		F: function(goal){
 			return this.G() + this.H(goal);
-		};
+		},
 
-		this.G = function(){
+		G: function(){
 			// if no cached value generate new one
-			if(!_g) {
+			if(!this._g) {
 				if (this.parent === undefined){
-					_g = 0;
+					this._g = 0;
 				}else{
-					_g = this.parent.G() + this.node.distance(this.parent.node);
+					this._g = this.parent.G() + this.node.distance(this.parent.node);
 				}
 			}
-			return _g;
-		};
+			return this._g;
+		},
 
-		this.H = function(goal){
+		H: function(goal){
 			return this.node.distance(goal);
-		};
+		},
 
-		this.path = function(){
+		path: function(){
 			if (this.parent === undefined){
 				return [this.node];
 			}else{
@@ -47,84 +49,84 @@ function PathFinder(options){
 				path.push(this.node);
 				return path;
 			}
-		};
+		},
 		
-		this.reParent = function(parent){
+		reParent: function(parent){
 			this.parent = parent;
 			//clear cached value
-			_g = null;
-		};
-	};
-
-}
-PathFinder.prototype.findpath = function(start, goal){
-	var adj, openlist = [];
-	var closedlist = this.lastclist = [];
-	var current = new PathNode(start);
-
-	
-	var nOpen = function(node){
-		openlist.push(node);
-	};
-
-	var nNext = function(){
-		var best = {F : function(){return Number.MAX_VALUE;}};
-		for (var i = 0; i < openlist.length; i++) {
-			if(openlist[i].F(goal) < best.F(goal)){
-				best = openlist[i];
-			}
+			this._g = null;
 		}
-		return best;
 	};
 
-	var nClose = function(node){
-		closedlist.push(node);
-		var i = openlist.indexOf(node);
-		openlist.splice(i,1);
-	};
+	this.findpath = function(start, goal){
+		var adj, openlist = [];
+		var closedlist = this.lastclist = [];
+		var current = new PathNode(start);
 
-	var inList = function(ent, i, ary) {
-		return this == ent.node;
-	};
+		
+		var nOpen = function(node){
+			openlist.push(node);
+		};
 
-
-	
-
-	nOpen(current);
-
-	do{
-		nClose(current);
-		adj = this.adjFunc(current.node);
-
-		for(var n in adj){
-			if(!closedlist.some(inList,adj[n])){
-				
-				var oents = openlist.filter(inList,adj[n]);
-				
-				if(oents.length > 0){
-					
-					old = oents[0];
-					nuw = new PathNode(adj[n], current);
-					
-					if(old.G() > nuw.G()){
-						old.reParent(current);
-					}
-
-				}else{
-
-					nOpen(new PathNode(adj[n], current));
-
+		var nNext = function(){
+			var best = {F : function(){return Number.MAX_VALUE;}};
+			for (var i = 0; i < openlist.length; i++) {
+				if(openlist[i].F(goal) < best.F(goal)){
+					best = openlist[i];
 				}
 			}
-		}
+			return best;
+		};
 
-		if (closedlist.some(inList, goal)){
-			return closedlist.filter(inList,goal)[0].path();
-		}
+		var nClose = function(node){
+			closedlist.push(node);
+			var i = openlist.indexOf(node);
+			openlist.splice(i,1);
+		};
 
-		current = nNext();
+		var inList = function(ent, i, ary) {
+			return this == ent.node;
+		};
 
-	}while(openlist.length > 0);
 
-	return false;
-};
+		
+
+		nOpen(current);
+
+		do{
+			nClose(current);
+			adj = this.adjFunc(current.node);
+
+			for(var n in adj){
+				if(!closedlist.some(inList,adj[n])){
+					
+					var oents = openlist.filter(inList,adj[n]);
+					
+					if(oents.length > 0){
+						
+						var old = oents[0];
+						var nuw = new PathNode(adj[n], current);
+						
+						if(old.G() > nuw.G()){
+							old.reParent(current);
+						}
+
+					}else{
+
+						nOpen(new PathNode(adj[n], current));
+
+					}
+				}
+			}
+
+			if (closedlist.some(inList, goal)){
+				return closedlist.filter(inList,goal)[0].path();
+			}
+
+			current = nNext();
+
+		}while(openlist.length > 0);
+
+		return false;
+	};
+}
