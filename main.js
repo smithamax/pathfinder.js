3e457cd70d7e46ba1ec85c822c5f8680b8f3c8ea
Dominic Smith <code@smithamax.me>
2012-11-16 18:39:38 +1100

    added semi colons for consistency

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.requestAnimFrame = (function(){
-	return	window.requestAnimationFrame       || 
-			window.webkitRequestAnimationFrame || 
-			window.mozRequestAnimationFrame    || 
-			window.oRequestAnimationFrame      || 
-			window.msRequestAnimationFrame     || 
+	return	window.requestAnimationFrame       ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
 			function(/* function */ callback, /* DOMElement */ element){
 				window.setTimeout(callback, 1000 / 60);
 			};
@@ -21,22 +21,22 @@ var showPaths = true;
 
 function init () {
 	can = document.createElement('canvas');
-	can.width = window.innerWidth
-	can.height = window.innerHeight-16
+	can.width = window.innerWidth;
+	can.height = window.innerHeight-16;
 	document.body.appendChild(can);
 	ctx = can.getContext('2d');
-	lasttime = Date.now()
+	lasttime = Date.now();
 	can.addEventListener('click',clicky,false);
 	//window.addEventListener('keydown',handleKey,false)
 
-	map = new Map(140,80)
+	map = new Map(140,80);
 
 	var gui = new dat.GUI();
-	var thing = {Pather:null}
+	var thing = {Pather:null};
 	var options = {
-		"Ajacent Neighbors": new PathFinder({edges:stra_adj,heuristic: function (a,b) {return Math.abs(a.x-b.x) + Math.abs(a.y-b.y)}}),
-		"evil": new PathFinder({edges:stra_adj,heuristic: function (a,b) {return Math.abs(a.x-b.x) + Math.abs(a.y-b.y)+1}}),
-		"evil2": new PathFinder({edges:stra_adj,heuristic: function (a,b) {return 0}}),
+		"Ajacent Neighbors": new PathFinder({edges:stra_adj,heuristic: function (a,b) {return Math.abs(a.x-b.x) + Math.abs(a.y-b.y);}}),
+		"evil": new PathFinder({edges:stra_adj,heuristic: function (a,b) {return Math.abs(a.x-b.x) + Math.abs(a.y-b.y)+1;}}),
+		"evil2": new PathFinder({edges:stra_adj,heuristic: function (a,b) {return 0;}}),
 		"Diaginal Neighbors":new PathFinder({edges:diag_adj,heuristic: function (a,b) {
 			var dx = Math.abs(a.x-b.x);
 			var dy = Math.abs(a.y-b.y);
@@ -44,8 +44,8 @@ function init () {
 			var shor = Math.abs(dx-dy);
 			return  diag + shor;
 		}}),
-		"shitty Neighbors": new PathFinder({edges:stra_adj}),
-	}
+		"shitty Neighbors": new PathFinder({edges:stra_adj})
+	};
 
 	pather = options["Ajacent Neighbors"];
 	
@@ -58,50 +58,51 @@ function init () {
 		pather = options[value];
 	});
 
-	gui.add(window, 'doLosslessCullNow')
-	gui.add(window, 'doDropNodeCullNow')
+	gui.add(window, 'doLosslessCullNow');
+	gui.add(window, 'doDropNodeCullNow');
 
-	ax = 2
-	ay = 2
-	dude = new Agent(GRID_SIZE*ax,GRID_SIZE*ay)
+	ax = 2;
+	ay = 2;
+	dude = new Agent(GRID_SIZE*ax,GRID_SIZE*ay);
 
 
-	tempmap = Generator.generate(21,21)
-	map.applyTemplete(tempmap,2)
+	tempmap = Generator.generate(21,21);
+	map.applyTemplete(tempmap,2);
 
 	
 	//map.randomiz()
 
-	loopsy()
+	loopsy();
 }
 
 
 var clicky = function(e){
-	var cx = Math.floor(e.clientX/GRID_SIZE) //needs to be fixed to offsetX equiv
-	var cy = Math.floor(e.clientY/GRID_SIZE)
-	var ax = Math.round(dude.pos.x/GRID_SIZE)
-	var ay = Math.round(dude.pos.y/GRID_SIZE)
+	var cx = Math.floor(e.clientX/GRID_SIZE); //needs to be fixed to offsetX equiv
+	var cy = Math.floor(e.clientY/GRID_SIZE);
+	var ax = Math.round(dude.pos.x/GRID_SIZE);
+	var ay = Math.round(dude.pos.y/GRID_SIZE);
 	if(editmode){
 		map.nodeAt(cx,cy).toggle();
 		map.dirty = true;
 	}else{
 		//start = goal;
-		start = map.nodeAt(ax,ay)
-		goal = map.nodeAt(cx,cy)
+		start = map.nodeAt(ax,ay);
+		goal = map.nodeAt(cx,cy);
 		if(start){
 			pather.findpath(start, goal, function(newpath){
+				if (!path) return;
 				path = newpath.slice(0);
 				if (doDropNodeCull){
-					path = dropNodeCull(path, function(x,y){return map.nodeAt(x,y).walkable})
+					path = dropNodeCull(path, function(x,y){return map.nodeAt(x,y).walkable;});
 				}
 				if (doLosslessCull){
-					path = losslessCull(path.slice(0))
+					path = losslessCull(path.slice(0));
 				}
-				dude.followPath(path.slice(0))
+				dude.followPath(path.slice(0));
 			});
 		}
 	}
-}
+};
 
 function handleKey(e){
 	var keynum;
@@ -133,27 +134,27 @@ PathFinder.prototype.drawClist = function(ctx){
 			ctx.lineTo((b.x+0.5)*GRID_SIZE,(b.y+0.5)*GRID_SIZE);
 		}
 		ctx.stroke();
-	};
+	}
 	ctx.restore();
-}
+};
 function drawPath(ctx,path){
-	ctx.save()
+	ctx.save();
 	ctx.strokeStyle = 'green';
 	ctx.lineWidth = 5.0;
 	ctx.lineCap = 'round';
 	ctx.beginPath();
 	if(path.length)
-		ctx.moveTo((path[0].x+0.5)*GRID_SIZE,(path[0].y+0.5)*GRID_SIZE)
+		ctx.moveTo((path[0].x+0.5)*GRID_SIZE,(path[0].y+0.5)*GRID_SIZE);
 	for (var i = 0; i < path.length; i++) {
 		ctx.fillStyle = 'lime';
 		ctx.fillRect(path[i].x*GRID_SIZE,path[i].y*GRID_SIZE,GRID_SIZE,GRID_SIZE);
-		ctx.lineTo((path[i].x+0.5)*GRID_SIZE,(path[i].y+0.5)*GRID_SIZE)
-	};
-	ctx.stroke()
-	ctx.restore()
+		ctx.lineTo((path[i].x+0.5)*GRID_SIZE,(path[i].y+0.5)*GRID_SIZE);
+	}
+	ctx.stroke();
+	ctx.restore();
 }
 
-var loopsy = function(){
+var loopsy = function () {
 	time = Date.now();
 	delta = time - lasttime;
 	lasttime = time;
@@ -161,24 +162,24 @@ var loopsy = function(){
 	map.draw(ctx);
 
 	if(showPaths){
-		pather.drawClist(ctx)
-		drawPath(ctx,path)
+		pather.drawClist(ctx);
+		drawPath(ctx,path);
 	}
 
-	ctx.save()
-	ctx.translate(dude.pos.x, dude.pos.y)
-	dude.update(delta)
-	dude.draw(ctx)
-	ctx.restore()
+	ctx.save();
+	ctx.translate(dude.pos.x, dude.pos.y);
+	dude.update(delta);
+	dude.draw(ctx);
+	ctx.restore();
 
-	requestAnimFrame(loopsy)
-}
+	requestAnimFrame(loopsy);
+};
 
 function doLosslessCullNow () {
 	path = losslessCull(path);
 }
 function doDropNodeCullNow (){
-	path = dropNodeCull(path, function(x,y){return map.nodeAt(x,y).walkable});
+	path = dropNodeCull(path, function(x,y){return map.nodeAt(x,y).walkable;});
 }
 
 window.onload = init;
